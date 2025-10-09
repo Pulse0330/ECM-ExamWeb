77c1f63bd754ba20f5fd7f663f7b662078270fbe
Bymbaajav Ganbold <ganboldbymbaaaaa@example.com>
2025-10-09 15:20:23 +0800

    Initial comm

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "@/app/(auth)/login/form";
+import IHeader from "@/components/IHeader";
+import React from "react";
+
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
